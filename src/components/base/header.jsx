f35b86a29c7267975fdc3f5ca38795ea3259f688
amabilee <dev.amabile@proton.me>
2024-10-17 19:06:00 -0300

    commit

--- src/components/base/header.jsx
+++ src/components/base/header.jsx
@@ -4,6 +4,9 @@ import bellIcon from '../../assets/header/bell-icon.svg'
 import ProfilePic from '../../assets/header/pfp.svg'
 import './style.css'
 
+import { SelectPicker } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
+
 
 function Header() {
     const [unities, setUnities] = useState(["Unidade Anápolis A", "Unidade Anápolis B"])
@@ -11,32 +14,49 @@ function Header() {
 
     const [userData, setUserData] = useState({ name: "José Figueiredo", profilePicture: ProfilePic })
 
-    return (
-        <div className="page-header">
-            <div className="page-header-left">
-                <img src={ConstractionLogo} />
-                <select
-                    value={selectedUnity}
-                    className='filtering-input'
-                    onChange={(e) => setSelectedUnity(e.target.value)}
-                >
-                    {unities.map((unity, i) => (
-                        <option key={i} value={unity}>{unity}</option>
-                    ))}
+    const navigationMainOptions = ['Gestão de Estoque', 'Gestão de Qualidade'].map(
+        item => ({ label: item, value: item })
+      );
 
-                </select>
-            </div>
-            <div className="page-header-right">
-                <div className="page-header-search">
-                    <input type="text" placeholder='Pesquisar' />
-                    <img src={bellIcon} />
+    const [navigationSelectedOption, setNavigationSelectedOption] = useState('Gestão de Estoque')
+
+    return (
+        <>
+            <div className="page-header">
+                <div className="page-header-left">
+                    <img src={ConstractionLogo} />
+                    <select
+                        value={selectedUnity}
+                        className='filtering-input'
+                        onChange={(e) => setSelectedUnity(e.target.value)}
+                    >
+                        {unities.map((unity, i) => (
+                            <option key={i} value={unity}>{unity}</option>
+                        ))}
+
+                    </select>
                 </div>
-                <div className="page-header-profile">
-                    <img src={userData.profilePicture} />
-                    <p>{userData.name}</p>
+                <div className="page-header-right">
+                    <div className="page-header-search">
+                        <input type="text" placeholder='Pesquisar' />
+                        <img src={bellIcon} />
+                    </div>
+                    <div className="page-header-profile">
+                        <img src={userData.profilePicture} />
+                        <p>{userData.name}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="page-navigation">
+                <SelectPicker
+                    data={navigationMainOptions}
+                    value={navigationSelectedOption}
+                    onChange={setNavigationSelectedOption}
+                    searchable={false}
+                    style={{ width: 300 }}
+                />
+            </div>
+        </>
     )
 }
 
--- src/pages/gestao-estoque/estoque/estoque.jsx
+++ src/pages/gestao-estoque/estoque/estoque.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PainelIcon from '../../../assets/gestao/painel-icon.svg'
 import './style.css'
 import '../../style.css'
@@ -6,15 +6,32 @@ import '../../style.css'
 import Header from '../../../components/base/header'
 
 function GestaoEstoqueEstoque() {
+    const [headerButtonState, setHeaderButtonState] = useState({buttonPainel: "active-button", buttonTabela: "desactive-button"})
+
+    const changeButtonState = (typeOfButton) => {
+        console.log(typeOfButton)
+        if (typeOfButton === "painel") {
+            console.log(1)
+            setHeaderButtonState({buttonPainel: "active-button", buttonTabela: "desactive-button"})
+        } else {
+            console.log(2)
+            setHeaderButtonState({buttonPainel: "desactive-button", buttonTabela: "active-button"})
+        }
+    }
+
     return (
         <div className="page-container">
-            <Header/>
+            <Header />
             <div className="page-content">
                 <div className="page-content-box">
                     <div className="page-content-header">
                         <h1>Estoque</h1>
-                        <button><img src={PainelIcon} />Painel</button>
-                        {/* <button><img src={PainelIcon} />Tabela</button> */}
+                        <button onClick={() => changeButtonState("painel")} className={headerButtonState.buttonPainel}>
+                            <img src={PainelIcon} />Painel
+                        </button>
+                        <button onClick={() => changeButtonState("tabela")} className={headerButtonState.buttonTabela}>
+                            <img src={PainelIcon} />Tabela
+                        </button>
                     </div>
                 </div>
             </div>
